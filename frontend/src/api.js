58a404d57ff04278c1b2e22fec22c3084681e0dd
Dimitris Frangiadakis <dimitris@frangiadakis.com>
2019-12-27 00:41:27 +0100

    Sets api endpoint in an env var

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 class SubmissionApi {
 
     static getSymbols() {
-        return axios.get('http://localhost:8000/symbols');
+        return axios.get(`${process.env.REACT_APP_API_URL}/symbols`);
     }
 
     static getData(symbol, startDate, endDate, email) {
-        return axios.get('http://localhost:8000/data', {
+        return axios.get(`${process.env.REACT_APP_API_URL}/data`, {
             params: {
                 symbol: symbol,
                 startDate: startDate.format('YYYY-MM-DD'),
